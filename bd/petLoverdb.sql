6aaf125883e12d0314b93e570b1c15b8f45bad77
Lizz25 <lizpevacacela@espol.edu.ec>
2022-09-06 10:53:15 -0500

    petloverdb

--- bd/petLoverdb.sql
+++ bd/petLoverdb.sql
@@ -13,8 +13,19 @@ CREATE TABLE IF NOT EXISTS eventos(
 );
 
 CREATE TABLE IF NOT EXISTS comentarios(
-	id_comentario INT PRIMARY KEY AUTO_INCREMENT,
+  id_comentario INT PRIMARY KEY AUTO_INCREMENT,
     id_evento INT,
     comentario VARCHAR(255),
     FOREIGN KEY (id_evento) REFERENCES eventos(id_evento)
 );
+
+INSERT INTO eventos(nombre,fecha, hora, lugar, likes, descripcion,url_imagen) VALUES 
+('Feria de perros','2022-08-21','12:21','Parque Samanes',0,'Feria de perros con tiendas para consentirlos','img1.jpg'),
+('Feria de gatos','2022-09-23','12:21','Parque Samanes',0,'Feria de perros con tiendas para consentirlos','img1.jpg'),
+('Feria de gatos y perros','2022-08-21','12:21','Parque Samanes',0,'Feria de perros con tiendas para consentirlos','img1.jpg');
+
+INSERT INTO eventos(id_evento,comentario) VALUES 
+(1,"Que evento tan increible, estoy ansioso por asistir"),
+(1,"Que evento tan increible, estoy ansioso por asistir :)"),
+(1,"Que evento tan increible, se ve muy organizado, estoy ansioso por asistir");
+
